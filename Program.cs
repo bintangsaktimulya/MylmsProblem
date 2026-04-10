bb5bd6c74385afc46cec11aac18613497fd4fba3
Bintang Sakti Mulya <bintangsaktimulya@gmail.com>
2026-04-10 07:58:29 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Student studentbwa = new Student("Rib", "12345@gmail.com", "112233", "Student", "XPG3");
+Teacher teacherbwa = new Teacher("hib", "123445@gmail.com", "12212233", "Teacher", "Math");
+studentbwa.display_info();
+teacherbwa.display_info();
+studentbwa.SubmitAssignment();
+teacherbwa.Grade();
+class Mylms
+{
+    public string Name;
+    public string Email;
+    public string Password;
+    public string Role;
+
+    public Mylms(string name, string email, string password, string role)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+    public void display_info()
+    {
+        Console.WriteLine($"Name: {Name}| Email: {Email}| Role: {Role}");
+    }
+}
+
+class Student : Mylms
+{
+    public string ClassName;
+
+    public Student(string name, string email, string password, string role, string className) : base(name, email, password, role)
+    {
+        ClassName = className;
+    }
+    public void SubmitAssignment()
+    {
+        Console.WriteLine($"Student {Name} has submitted assignment");
+    }
+}
+class Teacher : Mylms
+{
+    public string Subject;
+
+    public Teacher(string name, string email, string password, string role, string subject) : base(name, email, password, role)
+    {
+        Subject = subject;
+    }
+    public void Grade()
+    {
+        Console.WriteLine($"Teacher {Name} has graded assignment");
+    }
+}
